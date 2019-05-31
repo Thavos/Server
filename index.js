122228de25d8b74f466c4199f25bcdb0005f21c1
THAVOS\Simon <simon.hruz252@gmail.com>
2019-05-31 13:12:26 +0200

    123

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ const server = express()
 
 const io = socketIO(server);
 
+express()
+    .get('/', function(req, res){
+        res.send('Hi');
+    })
+
 io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
